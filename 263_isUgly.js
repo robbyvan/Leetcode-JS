69145fdbd71693249e3b64e034180bb8d1107bf7
robbyvan <fzpinus@gmail.com>
2017-02-02 21:37:06 -0800

    263

---
+++ 263_isUgly.js
@@ -0,0 +1,22 @@
+function isUgly(num){
+  if (num <= 0){
+    return false;
+  }
+  if (num === 1){
+    return true;
+  }
+  while (num % 2 === 0){
+    num = Math.floor(num / 2);
+  }
+  while (num % 3 === 0){
+    num = Math.floor(num / 3);
+  }
+  while (num % 5 === 0){
+    num = Math.floor(num / 5);
+  }
+  if (num === 1){
+    return true;
+  }else{
+    return false;
+  }
+}
