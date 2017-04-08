321159a3932ab90c9785b564550d654c4577e58a
robbyvan <fzpinus@gmail.com>
2017-04-08 12:34:36 -0700

    63. Unique Paths II

--- 63_Unique Paths II.js
+++ 63_Unique Paths II.js
@@ -2,38 +2,38 @@
  * @param {number[][]} obstacleGrid
  * @return {number}
  */
-var uniquePathsWithObstacles = function(obstacleGrid){
-    var rows = obstacleGrid.length;
-    var cols = obstacleGrid[0].length;
+const uniquePathsWithObstacles = (obstacleGrid) => {
+    let rows = obstacleGrid.length;
+    let cols = obstacleGrid[0].length;
 
     //create DP table
-    // var opt = new Array(rows + 1);
-    // for (var i = 0; i < opt.length; ++i) {
+    // let opt = new Array(rows + 1);
+    // for (let i = 0; i < opt.length; ++i) {
     //   opt[i] = new Array(cols + 1);
     // }
 
-    var opt = [];
-    for (var i = 0; i <= rows; ++i){
+    let opt = [];
+    for (let i = 0; i <= rows; ++i){
       opt[i] = [];
-      for (var j = 0; j <= cols; ++j) {
+      for (let j = 0; j <= cols; ++j) {
         opt[i][j] = undefined;
       }
     }
 
 
     //init
-    for (var i = 0; i <= rows; ++i) {
+    for (let i = 0; i <= rows; ++i) {
       opt[i][0] = 0;
     }
-    for (var j = 0; j <= cols; ++j) {
+    for (let j = 0; j <= cols; ++j) {
       opt[0][j] = 0;
     }
     
     opt[1][1] = 1;
 
     //recurrence
-    for (var i = 1; i <= rows; ++i) {
-      for (var j = 1; j <= cols; ++j) {
+    for (let i = 1; i <= rows; ++i) {
+      for (let j = 1; j <= cols; ++j) {
         if (obstacleGrid[i-1][j-1] === 1) {
           opt[i][j] = 0;
           continue;
